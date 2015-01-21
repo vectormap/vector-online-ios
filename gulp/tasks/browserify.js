0bdcde6f7abce0b4804a64f6def94abbf3b8e5e1
mbektimirov <mbektimirov@gmail.com>
2015-01-22 02:22:03 +0500

    Fixed out of memory error: 6to5/6to5#558

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -4,19 +4,18 @@ var bundleLogger = require('../util/bundleLogger');
 var gulp         = require('gulp');
 var handleErrors = require('../util/handleErrors');
 var source       = require('vinyl-source-stream');
-var _6to5        = require('6to5ify');
+var _6to5        = require('./6to5ify');
 var aliasify     = require('aliasify');
 var externalLibs = require('../external-libs');
 var cssify       = require('cssify');
 var reactify     = require('reactify');
+var buffer       = require('vinyl-buffer');
 
 gulp.task('browserify', function() {
 
 	var bundler = browserify({
     cache: {}, packageCache: {}, fullPaths: true,
-		// Specify the entry point of your app
 		entries: ['./src/scripts/app.jsx'],
-		// Add file extentions to make optional in your requires
 		extensions: ['.js', '.jsx', /*'.css', '.scss'*/],
 		debug: false, //!gulp.env.production, // enable source maps
     //global: true, // global transforms
@@ -25,6 +24,10 @@ gulp.task('browserify', function() {
     detectGlobals: false
 	});
 
+  bundler.transform(_6to5.configure({
+    ignore: 'node_modules',
+    sourceMap: false
+  }));
 
   var bundle = function() {
     // Log when bundling starts
@@ -32,25 +35,10 @@ gulp.task('browserify', function() {
 
     return bundler
       .external(externalLibs)
-      // .transform({global: true}, aliasify)
-      // .transform(reactify)
-      // .transform(cssify)
-      .transform(_6to5.configure({
-        sourceMap: 'inline'
-      }))
 			.bundle()
-      // Report compile errors
       .on('error', handleErrors)
-      // Use vinyl-source-stream to make the
-      // stream gulp compatible. Specifiy the
-      // desired output filename here.
       .pipe(source('app.js'))
-      // .pipe(changed('./build/', {extension: '.js'}))
-      // .pipe(debug({verbose: true}))
-      // .pipe(transform(function () { return exorcist('./build/app.js.map'); }))
-      // Specify the output destination
       .pipe(gulp.dest('./build/'))
-			// Log when bundling completes!
 			.on('end', bundleLogger.end);
 	};
 
