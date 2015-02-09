64ed92a0ee018ee596c885fad5f0922c2e89581e
mbektimirov <mbektimirov@gmail.com>
2015-02-09 16:10:49 +0500

    Popup modal animation

--- src/scripts/ui/MapView.jsx
+++ src/scripts/ui/MapView.jsx
@@ -1,6 +1,7 @@
-var React = require('react');
+var React = require('react/addons');
 var M     = require('morearty');
 var mapController = require('map-controller');
+var {CSSTransitionGroup} = React.addons;
 
 var MapView = React.createClass({
   mixins: [M.Mixin],
@@ -11,7 +12,20 @@ var MapView = React.createClass({
 
   render () {
     return (
-      <div ref="map" className="pane" />
+      <div>
+        <div ref="map" className="pane" />
+          <CSSTransitionGroup transitionName="vmp-modal" component="div">
+            {this.getBinding().get('modal') &&
+              <div className="modal vmp-modal" key={`modal-${Date.now()}`}>
+                <div className="bar bar-header disable-user-behavior">
+                  <h1 className="title">Geo popup [test]</h1>
+                  <div className="buttons">
+                    <button className="button button-clear" ng-click="closeLogin()">Отмена</button>
+                  </div>
+                </div>
+              </div>}
+          </CSSTransitionGroup>
+      </div>
     );
   }
 });
